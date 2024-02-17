60b36b6f89576a4f0a7d748a67ff68f7b8012713
tkj3nfuq <thinphcv5@gmail.com>
2024-02-17 14:58:05 +0700

    navbar update

--- src/app/ui/dashboard/navbar/page.tsx
+++ src/app/ui/dashboard/navbar/page.tsx
@@ -1,8 +1,25 @@
+"use client"
 import React from 'react'
+import { usePathname } from 'next/navigation'
+import styles from './navbar.module.css'
+import { MdAccountCircle  } from "react-icons/md";
 
 function NavBar() {
+
+  const pathName = usePathname();
+  
   return (
-    <div>NavBar</div>
+    <div className={styles.container}>
+      <div className={styles.title}>
+        {pathName.split('/').pop()}
+      </div>
+      <div className={styles.user}>
+        <div>UserName</div>
+        <div className={styles.spacing}>
+          <MdAccountCircle size={30}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
